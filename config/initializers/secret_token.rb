0caaf9425c2e0ca9b962793a816edccb65a4cb09
Ryan Alyn Porter <rap@endymion.com>
2010-12-03 14:12:25 -0500

    Initial commit to testapple.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapple::Application.config.secret_token = '74a91399a1de150a5182b57f808af196e6bbfd922081863eee9e5a2c7730ad0aee9ec108fb713e7b1c75a57d8b8713c930a7cb0edc474804142dad4555160a9c'
